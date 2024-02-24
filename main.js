38ce916b281e8d22c4b6a151c19432ba5e5da00b
chamath1991 <chamath.erplanka@gmail.com>
2024-02-24 20:03:21 +0530

    first commit of library

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function emailValidation(email){
+
+    let regExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regExp.test(email);
+}
+
+function capitalizeFirstWord(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+module.exports = emailValidation;
+// module.exports = capitalizeFirstWord;
