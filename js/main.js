87a4e5cf863555856c92585293031bd6e210613b
chamath1991 <chamath.erplanka@gmail.com>
2024-02-25 13:31:58 +0530

    error handlling

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-import {emailValidation} from './validations/emailValidation.js';
-import {capitalizeFirstWord} from './capitalizeFirstWord.js';
-import {dateToDateString, dateToLocalString, dateToTimestamp, timeStampToDate } from './dateFormatting/formatMainTypes/mainFormatTypesMain.js';
-import {formatDateCustom, formatTimeStampCusDate} from './dateFormatting/formatCustomTypes/mainFormatCustomTypes.js';
-import { getdate, getmonth, getyear } from './dateFormatting/getMDY/mainMDY.js';
-import { emailValidation, linkValidation } from './validations/mainValidationFile.js';
+// import {capitalizeFirstWord} from './capitalizeFirstWord.js';
+// import {dateToDateString, dateToLocalString, dateToTimestamp, timeStampToDate } from './dateFormatting/formatMainTypes/mainFormatTypesMain.js';
+// import {formatDateCustom, formatTimeStampCusDate} from './dateFormatting/formatCustomTypes/mainFormatCustomTypes.js';
+// import { getdate, getmonth, getyear } from './dateFormatting/getMDY/mainMDY.js';
+// import { emailValidation, linkValidation } from './validations/mainValidationFile.js';
 
-module.exports= {
-    emailValidation,
+export function capitalizeFirstWord(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+
+export {
+    // emailValidation,
     capitalizeFirstWord,
-    dateToTimestamp,
-    timeStampToDate,
-    dateToDateString,
-    dateToLocalString,
-    formatDateCustom,
-    formatTimeStampCusDate,
-    getdate,
-    getmonth,
-    getyear,
-    linkValidation
+    // dateToTimestamp,
+    // timeStampToDate,
+    // dateToDateString,
+    // dateToLocalString,
+    // formatDateCustom,
+    // formatTimeStampCusDate,
+    // getdate,
+    // getmonth,
+    // getyear,
+    // linkValidation
 } ; 
